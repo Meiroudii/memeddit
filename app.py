7d223334b853f5fce6b156c6b0d02030033f2663
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-03-25 22:36:00 +0800

    Integrate meme_api

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request
+import json
+import requests
+from waitress import serve
 
 app = Flask(__name__)
 
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    meme_pic,subreddit = get_meme()
+    return render_template("index.html", meme_pic=meme_pic, subreddit=subreddit)
--- meme_api.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_tempate
-import requests
-import json
-
-def get_meme():
-    url = "https://meme-api.com/gimme"
-    response = json.loads(requests.request("GET", url).text)
-    meme_large = response["preview"][-2]
